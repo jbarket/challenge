4e8dc47fb2a2ee913ecd49041c4ef7dac76c79ff
Jonathan Barket <jbarket@sleepunit.com>
2018-06-15 18:24:51 -0500

    Floor detection and movement for Elevator Actor

--- actors/elevator.js
+++ actors/elevator.js
@@ -1,4 +1,5 @@
 import '../messages/envelope';
+import 'pseudo/floorStatus';
 
 class Elevator {
 
@@ -7,7 +8,7 @@ class Elevator {
     a failure without losing vital information like how many trips have been made
     between services */
     
-    constructor(floorsTotal, floorsCurrent = 0, tripsMade = 0, floorsPassed = 0) {
+    constructor(floorsTotal, floorsCurrent = 1, tripsMade = 0, floorsPassed = 0) {
         if (!floorsTotal) {
             throw "Elevator did not receive required parameter floorsTotal. It is unsafe to run without knowing this.";
         }
@@ -16,16 +17,52 @@ class Elevator {
         this.floorsCurrent = floorsCurrent;
         this.tripsMade = tripsMade;
         this.floorsPassed = floorsPassed;
+
+        /* We'll start assuming that we're in the right place. */
+        this.floorsRequested = floorsCurrent;
+        this.floorsLastMove = null;
     }
 
-    /* We expect the message to specify the machine friendly format of the floor.
-    That is to say, if the user pushes the 1 button on the elevator, our expectation
-    is that we'll receive 0 here. */
+    /* To make the math simpler, we'll expect that the floors will be specified in human friendly format, rather
+    than starting at 0. */
 
     move(floor) {
-        if (floor > floorsTotal - 1) {
-            return 
+        if (!Number.isInteger(floor)) {
+            return new Envelope("InvalidFloor", "You must specify a whole number for the floor.")
         }
 
+        if (floor > floorsTotal) {
+            return new Envelope("InvalidFloor", "That floor is too high for this elevator. Please try the Wonkavator.");
+        }
+
+        if (floor < 1) {
+            return new Envelope("InvalidFloor", "That floor is too low. Look for basement support in an upcoming release.")
+        }
+
+        return true;
     }
+
+    /* For this to be accurate, we have to be able to retrieve the floor via instrumentation, like a switch that will be
+    tripped by the elevator car as it approaches each floor. This method is pseudo-code to handle messages from some
+    external library that's retrieving the current state of that switch. */
+
+    setFloor(floor) {
+
+        /* We may receive multiple signals that we are at the same floor. There's nothing to record for this. */
+        if (this.floorsCurrent == floor) {
+            return;
+        }
+
+        /* Otherwise, we've moved at least one floor. We expect this number to usually be 1, but because we're
+        receiving these messages externally and we don't want to ignore the possibility that a switch could be
+        disabled preventing us from detecting a particular floor, we always want to take the difference. */
+        this.floorsPassed += abs(this.floorsCurrent - floor);
+
+        this.floorsCurrent = floor;
+
+        if (this.floorsCurrent == this.floorsRequested) {
+            this.tripsMade += 1;
+        }
+    }    
+    
 }
